6c08ddca830ddf3f6e0919fa2b550491423958cd
rizkitafaja <rizkitafaja@gmail.com>
2022-09-01 07:19:51 +0700

    Alquran

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+
+<?php
+header('Content-Type: text/html; charset=utf-8');
+// Deployment Connection
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "dbquran2";
+
+// Heroku Deployment
+// $server = "remotemysql.com";
+// $user = "NINsC6rhHd";
+// $password = "3OypXumtTR";
+// $database = "NINsC6rhHd";
+$koneksi = mysqli_connect($server, $user, $password, $database) or die(mysqli_error(($koneksi)));
